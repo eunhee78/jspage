7e0346e89e2457ce80b9fd28ea895e56f8b4301f
eunhee78 <dmsgml4579@naver.com>
2024-04-04 17:46:30 +0900

    feat: create javascript01.js

---
+++ javascript01.js
@@ -0,0 +1,26 @@
+// 자바스크립트 변수, 자료형, 함수, 조건문, 반복문
+
+// 변수
+let coffee = "커피";
+let chocoBread = '"초코"빵';
+
+let camelCase; 카멜케이스
+let snake_case; 스네이크케이스
+let PascalCase; 파스칼케이스
+let $case1;
+let _case2;
+
+let bread = '빵';
+let creamBread = '크림빵';
+bread = creamBread;
+console.log(bread);
+console.log('첫번째 빵은 : ', bread);
+console.log(`두번째 빵은 : ${bread}`);
+
+// 자료형
+
+// 함수
+
+// 조건문
+
+// 반복문
